c2ddcbd26fb63819d1f93d73a5f0c3468d0c6a50
Ziaur Rahman <programmerzia@gmail.com>
2023-05-14 22:56:18 +0600

    removed other files.

--- public/index.php
+++ public/index.php
@@ -4,10 +4,9 @@ use app\core\Application;
 use app\controllers\SiteController;
 $app = new Application(dirname(__DIR__));
 
-//$app->router->get('/', function(){
-//    return 'Hello World!';
-//});
-$app->router->get('/', [\app\controllers\WeatherController::class,'weather']);
+$app->router->get('/', function(){
+    return 'Hello World!';
+});
 
 $app->router->get('/home', [SiteController::class,'home']);
 $app->router->post('/contact', [SiteController::class, 'handleSubmittedData']);
