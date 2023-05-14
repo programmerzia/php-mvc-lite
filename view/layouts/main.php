15c64793cf3a610b203d7579d9d0b1e0a54b7e0b
Ziaur Rahman <programmerzia@gmail.com>
2023-05-14 22:58:20 +0600

    removed other files.

--- view/layouts/main.php
+++ view/layouts/main.php
@@ -6,13 +6,11 @@
     <title>Weather Application</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../assets/css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
     {{ content }}
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="../../assets/js/script.js" ></script>
 </body>
 </html>
